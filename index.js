58fbec29c73541dff44a5ec4befb71679940288f
Julien Messer <julien.messer@laposte.net>
2022-12-27 17:14:03 +0100

    Just an editorial change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel bonjour monde");
+  res.send("Express on Vercel bonjour monde modif");
 });
 
 app.listen(5000, () => {
